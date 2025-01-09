c5706312da69f0b99078949e7c60b4258c10f3e3
santhoshraman7155 <santhoshraman7155@gmail.com>
2025-01-09 13:54:32 +0530

    Add akr.jpeg file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
             {/*  mobile menu */}
             <div className={` ${showMenu ? 'fixed w-1/2 bg-gray-950':'h-0 w-0'} md:hidden right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
                 <div className='flex items-center justify-between px-5 py-6 '>
-                    <img className='w-40 h-10 object-cover' src={assets.logo} alt="" />
+                    <img className='w-40 h-10 object-cover' src={assets.akr_logo} alt="" />
                     <img className='w-7' onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
                 </div>
                 <ul className='flex flex-col items-center gap-2 mt-5 text-lg font-medium'>
